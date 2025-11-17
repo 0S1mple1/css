39124ef940aab51edb7cda8cf354fc3771bd60b9
0S1mple1 <gabriel.lucasmodesto@hotmail.com>
2025-11-17 10:35:59 -0300

    Add dynamic inline styles to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
+import { useState } from "react";
+
 function App() {
+  const n = 15;
+  const [name] = useState("Gabriel");
   return (
     <div className="App">
       {/* CSS Global */}
@@ -16,6 +20,22 @@ function App() {
       <p style={{ color: "orange", backgroundColor: "lightgray" }}>
         Este é um parágrafo estilizado com CSS inline.
       </p>
+      {/* CSS Inline dinâmico */}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "red" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "red" }}>
+        CSS dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Gabriel"
+            ? { color: "green", backgroundColor: "#000" }
+            : null
+        }
+      >
+        Teste nome
+      </h2>
     </div>
   );
 }
